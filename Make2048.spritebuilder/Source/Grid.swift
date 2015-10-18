038ba8503fe1c63c5f6e9073fefecde2db3a1f21
daniele scochet <daniele@scochet.net>
2015-10-19 01:18:34 +0200

    Here we display two tiles, and move them.

--- Make2048.spritebuilder/Source/Grid.swift
+++ Make2048.spritebuilder/Source/Grid.swift
@@ -157,19 +157,106 @@ class Grid: CCNodeColor {
     }
     
     func swipeLeft() {
-        print("Left swipe!")
+        move(CGPoint(x: -1, y: 0))
     }
     
     func swipeRight() {
-        print("Right swipe!")
+        move(CGPoint(x: 1, y: 0))
     }
     
     func swipeUp() {
-        print("Up swipe!")
+        move(CGPoint(x: 0, y: 1))
     }
     
     func swipeDown() {
-        print("Down swipe!")
+        move(CGPoint(x: 0, y: -1))
+    }
+    
+    func move(direction: CGPoint) {
+        // apply negative vector until reaching boundary, this way we get the tile that is the farthest away
+        // bottom left corner
+        var currentX = 0
+        var currentY = 0
+        // Move to relevant edge by applying direction until reaching border
+        while indexValid(currentX, y: currentY) {
+            let newX = currentX + Int(direction.x)
+            let newY = currentY + Int(direction.y)
+            if indexValid(newX, y: newY) {
+                currentX = newX
+                currentY = newY
+            } else {
+                break
+            }
+        }
+        // store initial row value to reset after completing each column
+        var initialY = currentY
+        // define changing of x and y value (moving left, up, down or right?)
+        var xChange = Int(-direction.x)
+        var yChange = Int(-direction.y)
+        if xChange == 0 {
+            xChange = 1
+        }
+        if yChange == 0 {
+            yChange = 1
+        }
+        // visit column for column
+        while indexValid(currentX, y: currentY) {
+            while indexValid(currentX, y: currentY) {
+                // get tile at current index
+                if let tile = gridArray[currentX][currentY] {
+                    // if tile exists at index
+                    var newX = currentX
+                    var newY = currentY
+                    // find the farthest position by iterating in direction of the vector until reaching boarding of
+                    // grid or occupied cell
+                    while indexValidAndUnoccupied(newX+Int(direction.x), y: newY+Int(direction.y)) {
+                        newX += Int(direction.x)
+                        newY += Int(direction.y)
+                    }
+                    //After the loop terminates we check if the position of the selected tile has changed. For example, if the selected tile already is located left edge of the grid and we want to move it to the left, the position will not change. Only if the position changed we call the moveTile method.
+                    if newX != currentX || newY != currentY {
+                        moveTile(tile, fromX: currentX, fromY: currentY, toX: newX, toY: newY)
+                    }
+                }
+                // move further in this column
+                currentY += yChange
+            }
+            currentX += xChange
+            currentY = initialY
+        }
+    }
+    
+    // All this method does is checking wether the index is within the bounds of the two dimensional array.
+    func indexValid(x: Int, y: Int) -> Bool {
+        var indexValid = true
+        indexValid = (x >= 0) && (y >= 0)
+        if indexValid {
+            indexValid = x < Int(gridArray.count)
+            if indexValid {
+                indexValid = y < Int(gridArray[x].count)
+            }
+        }
+        return indexValid
+    }
+    
+    func indexValidAndUnoccupied(x: Int, y: Int) -> Bool {
+        let indexValid = self.indexValid(x, y: y)
+        if !indexValid {
+            return false
+        }
+        // unoccupied?
+        return gridArray[x][y] == noTile
+    }
+    
+    func moveTile(tile: Tile, fromX: Int, fromY: Int, toX: Int, toY: Int) {
+        gridArray[toX][toY] = gridArray[fromX][fromY]
+        gridArray[fromX][fromY] = noTile
+        let newPosition = positionForColumn(toX, row: toY)
+        let moveTo = CCActionMoveTo(duration: 0.2, position: newPosition)
+        // applico una easing al movimento
+        let easedAction = CCActionEaseOut(action: moveTo, rate: 3.0)
+        //tile.runAction(moveTo)
+        tile.runAction(easedAction)
     }
     
     /*
--- Make2048.spritebuilder/Source/Tile.swift
+++ Make2048.spritebuilder/Source/Tile.swift
@@ -9,7 +9,47 @@
 import Foundation
 
 class Tile: CCNode {
+    
+    /*
+    **************************************
+        properties
+    **************************************
+*/
+    
+    // CONNECTIONS
     // we always use the format weak var connectionName: ConnectionClass! for code connections to SpriteBuilder
     weak var valueLabel: CCLabelTTF!
     weak var backgroundNode: CCNodeColor!
+    
+    // stored properties
+    var value: Int = 0 {
+        // this is a property observer that updates the text of the label with the current value of the tile
+        didSet {
+            valueLabel.string = "\(value)"
+        }
+    }
+    
+    /*
+    **************************************
+        end of properties
+    **************************************
+    */
+
+
+
+    /*
+    **************************************
+        methods
+    **************************************
+    */
+    
+    func didLoadFromCCB() {
+        value = Int(CCRANDOM_MINUS1_1() + 2) * 2
+    }
+    
+    /*
+    **************************************
+        end of methods
+    **************************************
+    */
 }
